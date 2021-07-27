7aa1874dbfcefa29cb81cf162d9b7cbe24607e7a
NoahChinitzGWU <noahchinitz@gwmail.gwu.edu>
2021-07-26 21:02:02 -0400

    working version

--- cdf-histogram/cdf_histogram.py
+++ cdf-histogram/cdf_histogram.py
@@ -1,7 +1,3 @@
-# CDF-Histogram Bounty
-# Forked from https://github.com/twood02/bounties
-# Authors: Connor Burnett, Noah Chinitz, Aaron Hill
-
 import pandas as pd
 import numpy as np
 import matplotlib
@@ -19,20 +15,5 @@ else:
 
 column_names = ["Grades"]
 df = pd.read_csv('~/Bounties/bounties/cdf-histogram/grades.txt', delimiter = "\n", names = column_names)
-print(df)
-
-# np.random.seed(seed=42)
-# data_points = 1000
-
-# df = pd.DataFrame(data=list(zip(np.random.choice(["Math", "English"], size=data_points),
-#                                 np.random.beta(15, 10, size=data_points),
-#                                 np.random.beta(30, 4, size=data_points))),
-#             columns=['Major', 'Test1', 'Test2'])
-
-# df.head()
-
-# pd.DataFrame.hist(column="test")
-# pd.DataFrame.plot(kind='hist')
-# pd.DataFrame.plot.hist()
 
 df.hist(column="Grades")
